4127660e10814d86d53059cd30faee64d3f18ceb
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-05-27 18:53:41 +0530

    Complete the routing part of auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-const App = () => {
+import { Provider } from "react-redux";
+import SignUp from "./Components/Auth/SignUp";
+import SignIn from "./Components/Auth/SignIn";
+import Home from "./Pages/Home/HOme";
+
+
+const approute = createBrowserRouter([
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/",
+    element: <Home />, 
+    children: [
+     
+    ],
+  },
+]);
+
+function App() {
   return (
-    <div>
-      <h1 className='text-7xl font-bold text-pink-100'>hello world </h1>
-    </div>
-  )
+  
+      <RouterProvider router={approute} />
+   
+  );
 }
 
-export default App
+export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Home Compoenets
+    </div>
+  )
+}
+
+export default Home

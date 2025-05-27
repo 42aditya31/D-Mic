aa6dc550bde0e91cb79bbac32824b4921c279677
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-05-27 18:48:30 +0530

    Adding the structure for the D-Mic

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-7xl font-bold text-pink-100'>hello world </h1>
+    </div>
+  )
+}
+
+export default App
